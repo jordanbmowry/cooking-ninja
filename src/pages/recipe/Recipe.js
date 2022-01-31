85068e36cc2fdfd11e9aa94fb7d6b85e9d2fc44b
Jordan Mowry <jordan.mowry@gmail.com>
2022-01-31 14:04:20 -0700

    made changes to Recipe

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -17,7 +17,18 @@ export default function Recipe() {
     <div className='recipe'>
       {error && <p className='error'>{error}</p>}
       {isPending && <p className='loading'>Loading...</p>}
-      {recipe && <h1>{recipe.title}</h1>}
+      {recipe && (
+        <>
+          <h2 className='page-title'>{recipe.title}</h2>
+          <p>Take {recipe.cookingTime}</p>
+          <ul>
+            {recipe.ingredients.map((ingredient) => (
+              <li key={ingredient}>{ingredient}</li>
+            ))}
+          </ul>
+          <p className='method'>{recipe.method}</p>
+        </>
+      )}
     </div>
   );
 }
