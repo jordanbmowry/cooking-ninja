6fd4b9e598e13ac296c61700149403ab0007df34
Jordan Mowry <jordan.mowry@gmail.com>
2022-01-31 14:05:24 -0700

    wired up Create.js component to add a new recipe

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,5 +1,111 @@
-import './Create.css'
+import { useRef, useState } from 'react';
+import './Create.css';
 
 export default function Create() {
-  return <div>Create</div>;
+  const initalFormData = {
+    title: '',
+    method: '',
+    cookingTime: '',
+    ingredients: [],
+  };
+
+  const [formData, setFormData] = useState(initalFormData);
+  const [ingredient, setIngredient] = useState('');
+  const ingredientRef = useRef(null);
+
+  const handleFormChange = ({ target }) => {
+    const { name, value } = target;
+
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(formData);
+  };
+
+  const handleAddIngredient = (event) => {
+    event.preventDefault();
+    const ing = ingredient.trim().toLowerCase();
+
+    if (ing) {
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        ingredients: [...new Set([...prevFormData.ingredients, ing])],
+      }));
+    }
+    setIngredient('');
+    ingredientRef.current.focus();
+  };
+
+  return (
+    <div className='create'>
+      <h2 className='page-title'>Add a New Recipe</h2>
+      <form onSubmit={handleSubmit}>
+        <span>
+          <label htmlFor='title'>Recipe title:</label>
+          <input
+            name='title'
+            id='title'
+            type='text'
+            onChange={handleFormChange}
+            value={formData.title}
+            required
+          />
+        </span>
+        <span>
+          <label htmlFor='ingredients'>Recipe ingredients:</label>
+          <div className='ingredients'>
+            <input
+              name='ingredients'
+              id='ingredients'
+              type='text'
+              onChange={(event) => setIngredient(event.target.value)}
+              value={ingredient}
+              ref={ingredientRef}
+            />
+            <button
+              className='btn ingredients-btn'
+              onClick={handleAddIngredient}
+            >
+              add
+            </button>
+          </div>
+        </span>
+        <p>
+          Current ingredients:{' '}
+          {formData.ingredients.map((ingr) => (
+            <em key={ingr}>{ingr}, </em>
+          ))}
+        </p>
+        <span>
+          <label htmlFor='method'>Recipe method:</label>
+          <textarea
+            name='method'
+            id='method'
+            onChange={handleFormChange}
+            value={formData.method}
+            required
+          />
+        </span>
+        <span>
+          <label htmlFor='cookingTime'>Cooking time (minutes):</label>
+          <input
+            name='cookingTime'
+            id='cookingTime'
+            onChange={handleFormChange}
+            value={formData.cookingTime}
+            required
+            type='number'
+          />
+        </span>
+        <button className='btn' type='submit'>
+          submit
+        </button>
+      </form>
+    </div>
+  );
 }
