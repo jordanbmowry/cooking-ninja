9d98f25ac65d22b63e8bd58d8fa4a9b730ad98e6
Jordan Mowry <jordan.mowry@gmail.com>
2022-01-31 11:37:21 -0700

    made changes to Recipe to fetch individul recipie

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,5 +1,23 @@
+import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useFetch } from '../../hooks/useFetch ';
+// styles
 import './Recipe.css';
 
 export default function Recipe() {
-  return <div>Recipe</div>;
+  const { id } = useParams();
+
+  const {
+    data: recipe,
+    isPending,
+    error,
+  } = useFetch(`http://localhost:3000/recipes/${id}`);
+
+  return (
+    <div className='recipe'>
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {recipe && <h1>{recipe.title}</h1>}
+    </div>
+  );
 }
